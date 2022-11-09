de8382744b4861666909cdb45ab18f6b4019e31f
Dakshitha Lenagala <dakshithalenagala@gmail.com>
2022-11-09 11:34:29 +0200

    Add walls

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 class CELL_TYPE {
     static final char EMPTY = '.';
     static final char START = '>';
     static final char FINISH = '<';
+    static final char WALL = 'W';
 }
 
 public class Game {
     private int width, height;
     private char[][] grid;
+    private final int MAX_WALL_LENGTH = 5;
+    private final Set<String> occupied = new HashSet<>();
 
     public Game(int width, int height) {
         this.width = width;
@@ -34,12 +39,69 @@ public class Game {
         int startX = 0;
         int startY = getRandomInt(0, height);
         grid[startX][startY] = CELL_TYPE.START;
+        markOccupiedCell(startX, startY);
     }
 
     public void addFinish() {
         int finishX = width - 1;
         int finishY = getRandomInt(0, height);
         grid[finishX][finishY] = CELL_TYPE.FINISH;
+        markOccupiedCell(finishX, finishY);
+    }
+
+    public void addWall() {
+        int[] wallA = getUnoccupiedCell();
+        int[] wallB = getUnoccupiedCell();
+        while (true) {
+            if ((wallA[0] == wallB[0] && wallA[1] != wallB[1])) {
+                if (Math.abs(wallA[1] - wallB[1]) < MAX_WALL_LENGTH) {
+                    break;
+                }
+            } else if (wallA[0] != wallB[0] && wallA[1] == wallB[1]) {
+                if (Math.abs(wallA[0] - wallB[0]) < MAX_WALL_LENGTH) {
+                    break;
+                }
+            }
+            wallA = getUnoccupiedCell();
+            wallB = getUnoccupiedCell();
+        }
+
+        if (wallA[0] == wallB[0]) {
+            int wallX = wallA[0];
+            int wallStartY = Math.min(wallA[1], wallB[1]);
+            int wallEndY = Math.max(wallA[1], wallB[1]);
+            for (int y = wallStartY; y <= wallEndY; y++) {
+                grid[wallX][y] = CELL_TYPE.WALL;
+                markOccupiedCell(wallX, y);
+            }
+        } else {
+            int wallY = wallA[1];
+            int wallStartX = Math.min(wallA[0], wallB[0]);
+            int wallEndX = Math.max(wallA[0], wallB[0]);
+            for (int x = wallStartX; x <= wallEndX; x++) {
+                grid[x][wallY] = CELL_TYPE.WALL;
+                markOccupiedCell(x, wallY);
+            }
+        }
+    }
+
+    private void markOccupiedCell(int x, int y) {
+        String occupiedCoordinate = x + ":" + y;
+        occupied.add(occupiedCoordinate);
+    }
+
+    private boolean isCellOccupied(int x, int y) {
+        String checkCoordinate = x + ":" + y;
+        return occupied.contains(checkCoordinate);
+    }
+
+    private int[] getUnoccupiedCell() {
+        int [] cell = new int[2];
+        do {
+            cell[0] = getRandomInt(0, width);
+            cell[1] = getRandomInt(0, height);
+        } while (isCellOccupied(cell[0], cell[1]));
+        return cell;
     }
 
     private int getRandomInt(int min, int max) {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int width = 20, height = 10;
+        int width = 20, height = 10, walls = 5;
         Game game = new Game(width, height);
         game.addStart();
         game.addFinish();
+        for (int i = 0; i < walls; i++)
+            game.addWall();
         game.printGrid();
     }
 }
