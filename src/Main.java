2f37db0700044f3eefaba08d14786f2c204838d9
Dakshitha Lenagala <dakshithalenagala@gmail.com>
2022-11-09 13:08:42 +0200

    Get input from user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int width = 20, height = 10, walls = 5, spots = 10;
+        if (args.length != 4) {
+            System.out.println("Please provide inputs for width, height, walls and collapsing spots");
+            System.out.println("eg: java Main.java 10 20 5 20");
+            return;
+        }
+        int width = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
+        int walls = Integer.parseInt(args[2]);
+        int spots = Integer.parseInt(args[3]);
         Game game = new Game(width, height);
         game.addStart();
         game.addFinish();
