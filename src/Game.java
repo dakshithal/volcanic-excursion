f2a8bdedf24174f530c6101db62c056ba7510fb4
Dakshitha Lenagala <dakshithalenagala@gmail.com>
2022-11-09 10:57:18 +0200

    Use constants

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,11 @@
 import java.util.concurrent.ThreadLocalRandom;
 
+class CELL_TYPE {
+    static final char EMPTY = '.';
+    static final char START = '>';
+    static final char FINISH = '<';
+}
+
 public class Game {
     private int width, height;
     private char[][] grid;
@@ -10,7 +16,7 @@ public class Game {
         grid = new char[width][height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                grid[x][y] = '.';
+                grid[x][y] = CELL_TYPE.EMPTY;
             }
         }
     }
@@ -27,13 +33,13 @@ public class Game {
     public void addStart() {
         int startX = 0;
         int startY = getRandomInt(0, height);
-        grid[startX][startY] = '>';
+        grid[startX][startY] = CELL_TYPE.START;
     }
 
     public void addFinish() {
         int finishX = width - 1;
         int finishY = getRandomInt(0, height);
-        grid[finishX][finishY] = '<';
+        grid[finishX][finishY] = CELL_TYPE.FINISH;
     }
 
     private int getRandomInt(int min, int max) {
